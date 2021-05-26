6645f4bd173978ad64d8ee465798e85d60ebec50
KycTuK <djambroziya@gmail.com>
2021-05-26 05:40:43 +0300

    credit1

--- credit_calc/program.cs
+++ credit_calc/program.cs
@@ -1,70 +1,69 @@
 ﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
 
-namespace credit_calc
+Main(args);
+
+int GetYearDaysCount(int Year)
 {
-	class Program
+	return (DateTime.IsLeapYear(Year) ? 366 : 365);
+}
+int GetPeriodDaysCount(DateTime CurrentDate)
+{
+	return (CurrentDate.Subtract(CurrentDate.AddMonths(-1)).Days + 1);
+}
+
+double GetMonthAnnuityPayment(double Sum, double MonthCount, double Rate)
+{
+	double MonthRateCount = Math.Pow((1 + Rate), MonthCount);
+	return Sum * Rate * MonthRateCount / (MonthRateCount - 1);
+}
+double GetMonthPaymentPercent(double Sum, double Rate, int PeriodDaysCount, int YearDaysCount)
+{
+	return Sum * Rate * PeriodDaysCount / YearDaysCount;
+}
+/*
+double GetCurrentMonthCount(double TotalSum, double PaymentSum, double LoanRate)
+{
+	return (Math.Log(1 + LoanRate, PaymentSum / (PaymentSum - LoanRate * TotalSum)));
+}
+*/
+
+double GetSumReduce(double sum, double rate, double term, DateTime PaymentDate)
+{
+	Console.WriteLine("\t\t|{0:N2}\t\t|{0:N2}\t\t|{0:N2}",PaymentDate,);
+}
+
+void Main(string[] args)
+{
+
+	double	sum				=	1000000.0	;	//	Credit total summ
+	double	rate			=	12.0		;	//	Annual interest rate
+	int		term			=	10			;	//	Number of loan months
+	// int		selectedMonth	=	5			;	//	Number of month in which early payment was made
+	// double	payment			=	100000		;	//	Sum of early payment
+
+	rate /= 100;
+	DateTime	PaymentDate			=	DateTime.Now.AddDays(1 - DateTime.Now.Day);
+	double		MonthAnnuityPayment	=	GetMonthAnnuityPayment(sum, term, rate / 12);
+	double		Percentages			=	GetMonthPaymentPercent(sum, rate, GetPeriodDaysCount(PaymentDate), GetYearDaysCount(PaymentDate.Year));
+
+	Console.WriteLine("\t\t|Date\t\t|Payment\t\t|Percentages\t\t");
+
+	Console.WriteLine("Percentages: {0}",Percentages);
+	Console.WriteLine("MonthAnnuityPayment = {0:N2}",MonthAnnuityPayment);
+	Console.WriteLine("Переплата при уменьшении платежа: {0:N2}р.",sum - MonthAnnuityPayment);
+
+	Console.WriteLine("CurrentDate: {0}",PaymentDate);
+	Console.WriteLine("CurrentDate: {0}",PaymentDate.AddMonths(-1));
+	Console.WriteLine("GetPeriodDaysCount: {0}",GetPeriodDaysCount(PaymentDate));
+	Console.WriteLine("GetYearDaysCount: {0}",GetYearDaysCount(PaymentDate.Year));
+/*
+	int i = 4;
+	while (i++ > 0)
 	{
-		static int GetYearDaysCount(int Year)
-		{
-			return (DateTime.IsLeapYear(Year) ? 366 : 365);
-		}
-		static int GetPeriodDaysCount(DateTime CurrentDate)
-		{
-			return (CurrentDate.Subtract(CurrentDate.AddMonths(-1)).Days);
-		}
-		static double GetLoanRatePerMonth(double YearRate)
-		{
-			return (YearRate / 12 / 100);
-		}
-		static double GetMonthAnnuityCoefficient(double MonthCount, double MonthRate)
-		{
-			double MonthRateCount = Math.Pow((1 + MonthRate), MonthCount);
-			return (MonthRate * MonthRateCount / (MonthRateCount - 1));
-		}
-		static double GetMonthAnnuityPayment(double TotalSum, double AnnuityCoefficient)
-		{
-			return (TotalSum * AnnuityCoefficient);
-		}
-		static double GetCurrentMonthPaymentPercent(double TotalSum, double MonthRate, uint PeriodDaysCount, ushort DaysInYear)
-		{
-			return ((TotalSum * MonthRate * PeriodDaysCount) / (100));
-		}
-		static double GetCurrentMonthCount(double TotalSum, double PaymentSum, double LoanRate)
-		{
-			return (Math.Log(1 + LoanRate, PaymentSum / (PaymentSum - LoanRate * TotalSum)));
-		}
-
-		static void Main(string[] args)
-		{
-
-			double	sum				=	1000000.0	;	//	Credit total summ
-			double	rate			=	12.0		;	//	Annual interest rate
-			int		term			=	10			;	//	Number of loan months
-			// int		selectedMonth	=	5			;	//	Number of month in which early payment was made
-			// double	payment			=	100000		;	//	Sum of early payment
-
-			int		DaysInYear		=	GetYearDaysCount(DateTime.Now.Year);
-			double	MonthRate		=	GetLoanRatePerMonth(rate);
-			double	MonthAnnuityCoefficient	=	GetMonthAnnuityCoefficient(Convert.ToDouble(term), MonthRate);
-			double	MonthAnnuityPayment	=	GetMonthAnnuityPayment(sum, MonthAnnuityCoefficient);
-			// double	Percentages	=	GetCurrentMonthPaymentPercent(sum, MonthRate, 0, DaysInYear);
-
-			Console.WriteLine("MonthRate = {0}",MonthRate);
-			Console.WriteLine("MonthAnnuityCoefficient = {0}",MonthAnnuityCoefficient);
-			Console.WriteLine("MonthAnnuityPayment = {0}",MonthAnnuityPayment);
-			// Console.WriteLine("Percentages: {0}",Percentages);
-			Console.WriteLine("Переплата при уменьшении платежа: {0}р.",sum - MonthAnnuityPayment);
-
-			Console.WriteLine("CurrentDate: {0}",DateTime.Now);
-			Console.WriteLine("AddMonths(-1): {0}",DateTime.Now.AddMonths(-1));
-			Console.WriteLine("Subtract: {0}",GetPeriodDaysCount(DateTime.Parse("1 March 2007")));
-
-			// int i = 4;
-			// while (i++ > 0)
-			// {
-			// 	Percentages =
-			// }
-			// Console.WriteLine(" {0} ", GetMonthPaymentPercent(sum, MonthRate, PeriodDaysCount));
-		}
+		Percentages =
 	}
+	Console.WriteLine(" {0} ", GetMonthPaymentPercent(sum, MonthRate, PeriodDaysCount));
+*/
 }
